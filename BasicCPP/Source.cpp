0f420c104db268a85bf724d0c02f74bd1e580ae7
Коля <@DESKTOP-3O0178S>
2022-10-07 18:17:22 +0300

    new file

--- BasicCPP/Source.cpp
+++ BasicCPP/Source.cpp
@@ -3,5 +3,33 @@ using namespace std;
 
 int main()
 {
+	int namber;
+	cin >> namber;
+	switch (namber)
+	{
+	case 1:
+	{
+		cout << "“Hello, World!”" << endl;
+		cout << "/Bjarne Stroustrup/" << endl;
+
+	}break;
+	case 2:
+	{
+		cout << "Hello worsl!" << endl;
+		cout << "\\Dennis Ritchi\\" << endl;
+	}break;
+	case 3:
+	{
+		cout << "To be, or not to be" << endl;
+		cout << " that is the question" << endl;
+		cout << "Whether 'tis nobler in the mind to suffer" << endl;
+		cout << "The slings and arrows of outrageous fortune" << endl;
+		cout << "Or to take arms against a sea of troubles" << endl;
+		cout << "And by opposing end them? " << endl;
+		cout << "William Shakespeare" << endl;
+	}break;
+	default:
+		break;
+	}
 	return 0;
 }
