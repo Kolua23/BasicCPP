2ef59e6c87b979d552a76efdb3984c20bd0dacc6
Коля <@DESKTOP-3O0178S>
2022-10-11 14:48:44 +0300

    lab 4-1

--- BasicCPP/Source.cpp
+++ BasicCPP/Source.cpp
@@ -3,5 +3,31 @@ using namespace std;
 
 int main()
 {
+	int nam1, nam2;
+	setlocale(LC_ALL, "ukr");
+	srand(time(0));
+	cout << "Не менше ";
+	cin >> nam1;
+	cout << "Не більше ";
+	cin >> nam2;
+	int nam;
+	cin >> nam;
+	while (nam)
+	{
+
+		if  (nam1 || nam2)
+		{
+			cout << " діапазон ";
+			if (nam < nam1)
+				cout << "+" << endl;
+			else if (nam > nam2)
+				cout << "-" << endl;
+			else
+				cout << "win" << endl;
+			system("pause");
+			system("cls");
+			return 0;
+		}
+	}
 	return 0;
 }
