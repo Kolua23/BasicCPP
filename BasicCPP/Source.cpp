9bb4d8adba21873db1ea6276594f90d7e83627e2
Коля <@DESKTOP-3O0178S>
2022-11-02 17:29:25 +0200

    покажчики

--- BasicCPP/Source.cpp
+++ BasicCPP/Source.cpp
@@ -1,8 +1,103 @@
 #include<iostream>
+#include<ctime>
+
 using namespace std;
 
 int main()
 {
-	cin.get();
-	return 0;
+	int nam;
+	cout << "nam is ";
+	cin >> nam;
+	switch (nam)
+	{
+
+	case 3:
+	{
+		srand(time(0));
+		int size;
+		cout << "Enter size:";
+		cin >> size;
+		int* arr = new int[size];
+		int* arrcopy = &*arr;
+		for (size_t i = 0; i < size; i++)
+		{
+			arr[i] = rand() % 10;
+			cout << arr[i] << "\t";
+
+		}
+		delete[] arr;
+
+		cout << endl;
+		for (size_t i = 0; i < size; i++)
+		{
+			arrcopy[i] = rand() % 10;
+			cout << arrcopy[i] << "\t";
+
+		}
+
+		cout << endl;
+		int* arrcopy = &*arr;
+		for (size_t i = size - 1; i > 0; --i)
+		{
+
+			cout << *&arrcopy[i] << "\t";
+
+		}
+		delete[] arrcopy;
+	}break;
+	case 1:
+	{
+		srand(time(0));
+		int size;
+		cout << "Enter size:";
+		cin >> size;
+		int* arr = new int[size];
+		int* arrcopy = &*arr;
+		for (size_t i = 0; i < size; i++)
+		{
+			arr[i] = rand() % 10;
+			cout << arr[i] << "\t";
+
+		}
+		delete[] arr;
+
+		cout << endl;
+		for (size_t i = 0; i < size; i++)
+		{
+			arrcopy[i] = rand() % 10;
+			cout << arrcopy[i] << "\t";
+
+		}
+
+		cout << endl;
+	}break;
+	case 2:
+	{
+		srand(time(0));
+		int size;
+		cout << "Enter size:";
+		cin >> size;
+		int* arr = new int[size];
+		for (size_t i = 0; i < size; i++)
+		{
+			arr[i] = rand() % 10;
+			cout << arr[i] << "\t";
+
+		}
+		cout << endl;
+		int* arrcopy = &*arr;
+
+		for (size_t i = size - 1; i > 0; --i)
+		{
+
+			cout << *&arrcopy[i] << "\t";
+
+		}
+		delete[] arrcopy;
+
+	}break;
+	default:
+		break;
+	}
+
 }
